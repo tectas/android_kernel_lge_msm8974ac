0638857f4ca7003afe348afd60bf0cdacc61bd0e
Dennis Rassmann <showp1984@gmail.com>
2013-11-11 00:03:47 +0100

    msm_mpdecision: fix definitions for earlysuspend/lcd callback switch
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/msm_mpdecision.c
    
    Change-Id: Ic4037a1396774383a334bf1d044847ce4a1ab649

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -615,7 +615,7 @@ static int fb_notifier_callback(struct notifier_block *this,
 
 	return 0;
 }
-#elif defined CONFIG_HAS_EARLYSUSPEND
+#else
 static void msm_mpdec_early_suspend(struct early_suspend *h) {
        msm_mpdec_suspend();
 }
