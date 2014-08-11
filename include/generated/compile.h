abf208778e9080fbf0b9042aed70cc342a41e80c
Tectas <adrian.stoeckl@gmail.com>
2014-08-11 15:42:08 +0200

    Cleanup of generated files

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 5 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Tue Aug 5 15:53:26 CEST 2014"
-#define LINUX_COMPILE_BY "tectas"
-#define LINUX_COMPILE_HOST "tectas-rogg750jx"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.4.0-perf-g4289535-dirty"
