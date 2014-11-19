c1cc90042eb0769e72d932ce6f50ada169a268aa
Tectas <adrian.stoeckl@gmail.com>
2014-11-19 15:34:43 +0100

    fs: namespace: Fix uninitialized warning

--- fs/namespace.c
+++ fs/namespace.c
@@ -2360,11 +2360,11 @@ EXPORT_SYMBOL(mount_subtree);
 SYSCALL_DEFINE5(mount, char __user *, dev_name, char __user *, dir_name,
 		char __user *, type, unsigned long, flags, void __user *, data)
 {
-	int ret;
-	char *kernel_type;
-	char *kernel_dir;
-	char *kernel_dev;
-	unsigned long data_page;
+	int ret = 0;
+	char *kernel_type = NULL;
+	char *kernel_dir = NULL;
+	char *kernel_dev = NULL;
+	unsigned long data_page = 0;
 
 	ret = copy_mount_string(type, &kernel_type);
 	if (ret < 0)
