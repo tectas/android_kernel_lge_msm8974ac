9361ba3de9f8af483c2de6a41aa2fb3592cb025c
Paul Reioux <reioux@gmail.com>
2014-11-20 11:24:57 -0600

    cpufreq_intellimm: intelliminmax cpu governor initial baseline commit
    intelliminmax (intellimm) governor is designed to work with the newer SOCs with fixed voltage rails (ie MSM8974+ SOCs).  It is designed to work within those
    fixed voltage ranges in order to maximize battery performance while creating a
    smooth UI operations.
    
    Copyright 2014 Paul Reioux <aka faux123>
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: Intellimm: remove unused code and change tblmap algorithm
    
    tblmap: add power-save frequencies for the first 5 entries
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: more tweaks and table clean up
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: more tweaks
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: clean up derpy logic
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: final clean up and algorithm tweaks
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: finalizing part 2
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: finalizing part 3
    
    add back burst for smoother UI
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    intellimm: fix div by zero derp
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -411,6 +411,9 @@ extern struct cpufreq_governor cpufreq_gov_smartassv2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
+extern struct cpufreq_governor cpufreq_gov_intellimm;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
 #endif
 
 
