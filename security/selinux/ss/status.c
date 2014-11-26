c3ff8458e30642bf95741c8a0a1a4bf99a21395b
Tectas <adrian.stoeckl@gmail.com>
2014-11-26 21:03:04 +0100

    security/selinux: Enforce selinux permissive (TEMP)

--- security/selinux/ss/status.c
+++ security/selinux/ss/status.c
@@ -58,7 +58,7 @@ struct page *selinux_kernel_status_page(void)
 
 			status->version = SELINUX_KERNEL_STATUS_VERSION;
 			status->sequence = 0;
-			status->enforcing = selinux_enforcing;
+			status->enforcing = 0;
 			/*
 			 * NOTE: the next policyload event shall set
 			 * a positive value on the status->policyload,
