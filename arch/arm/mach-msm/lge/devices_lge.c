ad6b76a9a8f0aa6bcc3b48efde0e81f527f65bbb
Skin1980 <skin1980@gmail.com>
2015-01-13 15:36:37 +0100

    Added Kexec Hardboot Patch

--- arch/arm/mach-msm/lge/devices_lge.c
+++ arch/arm/mach-msm/lge/devices_lge.c
@@ -17,6 +17,10 @@
 #include <mach/lge_handle_panic.h>
 #endif
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <linux/memblock.h>
+#endif
+
 #ifdef CONFIG_LGE_PM
 #include <linux/qpnp/qpnp-adc.h>
 #include <mach/board_lge.h>
@@ -207,6 +211,16 @@ void __init lge_add_persist_ram_devices(void)
 
 void __init lge_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	struct memtype_reserve *mt = &reserve_info->memtype_reserve_table[MEMTYPE_EBI1];
+	phys_addr_t start = mt->start - SZ_1M - LGE_PERSISTENT_RAM_SIZE;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+	pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+	pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif	
+
 #if defined(CONFIG_ANDROID_PERSISTENT_RAM)
 	lge_add_persist_ram_devices();
 #endif
